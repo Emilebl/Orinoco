381419cc11e16b4dd226ee4822fca51e7e100b3b
Emile Bl <bailay.emile@live.fr>
2020-04-18 18:21:54 +0200

    CART object and functions created using the localStorage

--- scripts/selectedproductpage.js
+++ scripts/selectedproductpage.js
@@ -42,39 +42,266 @@ request.onload = function() {
             personalisationDisplay.appendChild(option);
             option.value = lense;
             option.innerHTML = lense;
-        })
+        });
+        const CART = {
+            KEY: 'rfrgrdg45dg15drg1dr',
+            contents: [],
+            init(){ 
+                // check localStorage and initialize the contents of CART.contents
+                let _contents = localStorage.getItem(CART.KEY);
+                if(_contents) {
+                    CART.contents = JSON.parse(_contents);
+                } else {
+                    // possible to use dummy data in the array
+                    CART.contents = [];
+                    CART.sync();
+                }
+            },
+            async sync() {
+                let _cart = JSON.stringify(CART.contents);
+                await localStorage.setItem(CART.KEY, _cart);
+            },
+            find(myId){
+                // find an item in the cart by its id (some"id" might be changed according to its key in the back end or here (myId ?))
+                let match = CART.contents.filter(item =>{
+                    if(item.id == myId)
+                    return true;
+                });
+                if(match && match[0])
+                return match[0];
+            },
+            add(myId){
+                // add a new item in the cart
+                // check if the item is not already in the cart
+                if (CART.find(myId)){
+                    CART.increase(myId, 1);
+                } else {
+                    // in the exemple it looks for which item we want with a filter method looking all the products from the database searching for a matching id, 
+                    // but in our case it is only the product that is currently displayed on the page
+                    let obj = {
+                        id: myId,
+                        title: camera.name,
+                        qty: 1,
+                        itemPrice: camera.price,
+                    };
+                    // push the object to the contents of CART
+                    CART.contents.push(obj);
+                    // update localStorage
+                    CART.sync();
+                }
+            },
+            increase(myId, qty=1){
+                // increase the quantity of an item in the cart
+                CART.contents = CART.contents.map(item=>{
+                    if(item.id === myId)
+                    item.qty = item.qty + qty;
+                    return item
+                });
+                // update localStorage
+                CART.sync();
+            },
+            reduce(myId, qty=1){
+                // increase the quantity of an item in the cart
+                CART.contents = CART.contents.map(item=>{
+                    if(item.id === myId)
+                    item.qty = item.qty - qty;
+                    return item
+                });
+                // remove item from cart if the qty is 0
+                CART.contents.forEach(async items => {
+                    if(item.id === myId && item.qty === 0)
+                    CART.remove(myId);
+                });
+                // update localStorage
+                CART.sync();
+            },
+            remove(myId){
+                CART.contents = CART.contents.filter(item =>{
+                    if(item.id !== myId)
+                    return true;
+                });
+                // update localStorage
+                CART.sync();
+            },
+            empty(){
+                // empty whole cart
+                CART.contents = [];
+                // update localStorage
+                CART.sync();
+            }
+        };
+        CART.init();
+        console.log(CART.contents);
+        addToCartButton.addEventListener("click", function() {
+            CART.add(myId);
+            console.log(CART.contents);
+        });
+
     }    
 };
 var myLocalHost = "http://localhost:3000/api/cameras/" + myId;
 request.open("GET", myLocalHost);
 request.send();
 
+// Création du panier avec un objet "CART", ainsi que toutes ses fonctionnalités
+
+// const CART = {
+//     KEY: 'rfrgrdg45dg15drg1dr',
+//     contents: [],
+//     init(){ 
+//         // check localStorage and initialize the contents of CART.contents
+//         let _contents = localStorage.getItem(CART.KEY);
+//         if(_contents) {
+//             CART.contents = JSON.parse(_contents);
+//         } else {
+//             // possible to use dummy data in the array
+//             CART.contents = [];
+//             CART.sync();
+//         }
+//     },
+//     async sync() {
+//         let _cart = JSON.stringify(CART.contents);
+//         await localStorage.setItem(CART.KEY, _cart);
+//     },
+//     find(myId){
+//         // find an item in the cart by its id (some"id" might be changed according to its key in the back end or here (myId ?))
+//         let match = CART.contents.filter(item =>{
+//             if(item.id == myId)
+//             return true;
+//         });
+//         if(match && match[0])
+//         return match[0];
+//     },
+//     add(myId){
+//         // add a new item in the cart
+//         // check if the item is not already in the cart
+//         if (CART.find(myId)){
+//             CART.increase(myId, 1);
+//         } else {
+//             // in the exemple it looks for which item we want with a filter method looking all the products from the database searching for a matching id, 
+//             // but in our case it is only the product that is currently displayed on the page
+//             let obj = {
+//                 id: myId,
+//                 title: camera.name,
+//                 qty: 1,
+//                 itemPrice: camera.price,
+//             };
+//             // push the object to the contents of CART
+//             CART.contents.push(obj);
+//             // update localStorage
+//             CART.sync();
+//         }
+//     },
+//     increase(myId, qty=1){
+//         // increase the quantity of an item in the cart
+//         CART.contents = CART.contents.map(item=>{
+//             if(item.id === myId)
+//             item.qty = item.qty + qty;
+//             return item
+//         });
+//         // update localStorage
+//         CART.sync();
+//     },
+//     reduce(myId, qty=1){
+//         // increase the quantity of an item in the cart
+//         CART.contents = CART.contents.map(item=>{
+//             if(item.id === myId)
+//             item.qty = item.qty - qty;
+//             return item
+//         });
+//         // remove item from cart if the qty is 0
+//         CART.contents.forEach(async items => {
+//             if(item.id === myId && item.qty === 0)
+//             CART.remove(myId);
+//         });
+//         // update localStorage
+//         CART.sync();
+//     },
+//     remove(myId){
+//         CART.contents = CART.contents.filter(item =>{
+//             if(item.id !== myId)
+//             return true;
+//         });
+//         // update localStorage
+//         CART.sync();
+//     },
+//     empty(){
+//         // empty whole cart
+//         CART.contents = [];
+//         // update localStorage
+//         CART.sync();
+//     }
+// }
+// initialisation du panier au chargement de la page
+document.addEventListener("load", function(){
+    CART.init();
+    console.log(CART.contents);
+})
+
+// Ajout d'un item au panier lors du click sur le bouton "ajouter au panier"
+// addToCartButton.addEventListener("click", function() {
+//     CART.add(myId);
+//     console.log(CART.contents);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Création de la fonctionnalité d'ajout au panier (nombre d'item dans le panier + prix total du panier)
-const itemCounter = document.getElementById("itemCounter");
+// const itemCounter = document.getElementById("itemCounter");
 
-var cartItems = [];
+// var cartItems = [];
 
-console.log(cartItems);
+// console.log(cartItems);
 
-addToCartButton.addEventListener("click", function addToCart(item) {
-    cartItems.push(item);
-    itemCounter.innerHTML = cartItems.length;
-    var nbItems = cartItems.length;
-    console.log(nbItems);
+// addToCartButton.addEventListener("click", function addToCart(item) {
+//     cartItems.push(item);
+//     itemCounter.innerHTML = cartItems.length;
+//     var nbItems = cartItems.length;
+//     console.log(nbItems);
 
-    //Appel d'une fonction externe pour passer le paramètre
-    cartLink.addEventListener("click", addIdToUrl(nbItems));
+//     //Appel d'une fonction externe pour passer le paramètre
+//     cartLink.addEventListener("click", addIdToUrl(nbItems));
     
-});
+// });
 
-const cartLink = document.getElementById("cartLink");
+// const cartLink = document.getElementById("cartLink");
 
 
 
-function addIdToUrl(nbItems) {
-    var myId = getElement();
-    var attribute = cartLink.setAttribute("href","cart.html?nb="+nbItems+"&id="+myId);
-    console.log(attribute);
-    console.log(myId + "cartLink");
+// function addIdToUrl(nbItems) {
+//     var myId = getElement();
+//     var attribute = cartLink.setAttribute("href","cart.html?nb="+nbItems+"&id="+myId);
+//     console.log(attribute);
+//     console.log(myId + "cartLink");
+// }
 
-}
+
+    
+
+
+
