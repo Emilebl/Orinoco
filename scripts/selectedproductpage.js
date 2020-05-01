31686a2019419657e29e84c7f932ffb326731b8c
Emile Bl <bailay.emile@live.fr>
2020-05-01 17:17:51 +0200

    API Request wrapped in promise - on product page

--- scripts/selectedproductpage.js
+++ scripts/selectedproductpage.js
@@ -1,3 +1,96 @@
+// Déclaration de l'objet CART pour le panier, avec ses fonctionnalités.
+const CART = {
+    KEY: 'rfrgrdg45dg15drg1dr',
+    contents: [],
+    // Fonction pour initialiser le panier
+    init(){ 
+        // check le contenu du localStorage et initialise le contenu de CART.contents
+        let _contents = localStorage.getItem(CART.KEY);
+        if(_contents) {
+            // Si il y a déjà des données: on les récupère
+            CART.contents = JSON.parse(_contents);
+        } else {
+            // Sinon on crée un array vide, et on synchronise notre objet JS avec le localStorage
+            CART.contents = [];
+            CART.sync();
+        }
+    },
+    // Fonction pour synchroniser "CART.contents" qui est un array et l'équivalent "string" dans le localStorage
+    async sync() {
+        let _cart = JSON.stringify(CART.contents);
+        await localStorage.setItem(CART.KEY, _cart);
+    },
+    // Trouve un item dans le panier en fonction de son id
+    find(myId){
+        let match = CART.contents.filter(item =>{
+            if(item.id == myId)
+            return true;
+        });
+        if(match && match[0])
+        return match[0];
+    },
+// Ajout d'un produit dans le panier
+    add(myId, camera){
+        // Vérifie si l'id du panier à ajouter n'est pas déjà présente dans l'array, avec la fonction find
+        if (CART.find(myId)){
+            // Si oui, on execute la fonction increase (pour augmenter la quantité)
+            CART.increase(myId, 1);
+        } else { 
+            // Sinon, création d'un nouvel objet pour le produit à ajouter
+            let obj = {
+                id: myId,
+                title: camera.name,
+                image: camera.imageUrl,
+                qty: 1,
+                itemPrice: camera.price/100,
+            };
+            // Ajout de l'objet dans l'array CART.contents
+            CART.contents.push(obj);
+            // Mise à jour du localStorage avec sync
+            CART.sync();
+        }
+    },
+    // Fonction pour augmenter de 1 la quantité d'un produit
+    increase(myId, qty=1){
+        CART.contents = CART.contents.map(item=>{
+            if(item.id === myId)
+            item.qty = item.qty + qty;
+            return item
+        });
+        // Mise à jour du localStorage avec sync
+        CART.sync();
+    },
+    // reduce(myId, qty=1){
+    //     // increase the quantity of an item in the cart
+    //     CART.contents = CART.contents.map(item=>{
+    //         if(item.id === myId)
+    //         item.qty = item.qty - qty;
+    //         return item
+    //     });
+    //     // remove item from cart if the qty is 0
+    //     CART.contents.forEach(async items => {
+    //         if(item.id === myId && item.qty === 0)
+    //         CART.remove(myId);
+    //     });
+    //     // update localStorage
+    //     CART.sync();
+    // },
+    // remove(myId){
+    //     CART.contents = CART.contents.filter(item =>{
+    //         if(item.id !== myId)
+    //         return true;
+    //     });
+    //     // update localStorage
+    //     CART.sync();
+    // },
+    // Fonction pour vider le panier
+    empty(){
+        CART.contents = [];
+        // Mise à jour du localStorage avec sync
+        CART.sync();
+    }
+};
+
 
 // Recupération des éléments du DOM afin de les modifier ensuite
 const specification = document.getElementById("specification");
@@ -9,7 +102,7 @@ const personalisationName = document.getElementById("personalisation-name");
 const personalisationDisplay = document.getElementById("lenses");
 const addToCartButton = document.createElement("button");
 
-// Recupération de l'id du produit selectionné, dans l'url de la page actuelle
+// Recupération de l'id du produit selectionné, depuis l'url de la page actuelle
 function getURL() {
     return window.location.href;
 }
@@ -24,226 +117,60 @@ function getElement() {
 var myId = getElement();
 
 // Récupération du produit correspondant à l'id récupéré + affichage des propriétés spécifiques au produit
-var request = new XMLHttpRequest();
-request.onload = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var camera = JSON.parse(this.responseText);
-        imageDisplay.src = camera.imageUrl;
-        nameDisplay.innerHTML = camera.name;
-        priceDisplay.innerHTML = camera.price/100 + " €";
-        descriptionDisplay.innerHTML = "<span>Description:</span><br>" + camera.description;
-        addToCartButton.innerHTML = "Ajouter au panier";
-        specification.appendChild(addToCartButton);
-        var cameraLenses = camera.lenses;
-        cameraLenses.forEach((lense) => {
-            var option = document.createElement("option");
-            option.appendChild(document.createTextNode('New Option Text'));
-            personalisationName.innerHTML = "Choix de la lentille:";
-            personalisationDisplay.appendChild(option);
-            option.value = lense;
-            option.innerHTML = lense;
-        });
-        const CART = {
-            KEY: 'rfrgrdg45dg15drg1dr',
-            contents: [],
-            init(){ 
-                // check localStorage and initialize the contents of CART.contents
-                let _contents = localStorage.getItem(CART.KEY);
-                if(_contents) {
-                    CART.contents = JSON.parse(_contents);
-                } else {
-                    // possible to use dummy data in the array
-                    CART.contents = [];
-                    CART.sync();
-                }
-            },
-            async sync() {
-                let _cart = JSON.stringify(CART.contents);
-                await localStorage.setItem(CART.KEY, _cart);
-            },
-            find(myId){
-                // find an item in the cart by its id (some"id" might be changed according to its key in the back end or here (myId ?))
-                let match = CART.contents.filter(item =>{
-                    if(item.id == myId)
-                    return true;
-                });
-                if(match && match[0])
-                return match[0];
-            },
-            add(myId){
-                // add a new item in the cart
-                // check if the item is not already in the cart
-                if (CART.find(myId)){
-                    CART.increase(myId, 1);
-                } else {
-                    // in the exemple it looks for which item we want with a filter method looking all the products from the database searching for a matching id, 
-                    // but in our case it is only the product that is currently displayed on the page
-                    let obj = {
-                        id: myId,
-                        title: camera.name,
-                        image: camera.imageUrl,
-                        qty: 1,
-                        itemPrice: camera.price/100,
-                    };
-                    // push the object to the contents of CART
-                    CART.contents.push(obj);
-                    // update localStorage
-                    CART.sync();
-                }
-            },
-            increase(myId, qty=1){
-                // increase the quantity of an item in the cart
-                CART.contents = CART.contents.map(item=>{
-                    if(item.id === myId)
-                    item.qty = item.qty + qty;
-                    return item
-                });
-                // update localStorage
-                CART.sync();
-            },
-            reduce(myId, qty=1){
-                // increase the quantity of an item in the cart
-                CART.contents = CART.contents.map(item=>{
-                    if(item.id === myId)
-                    item.qty = item.qty - qty;
-                    return item
-                });
-                // remove item from cart if the qty is 0
-                CART.contents.forEach(async items => {
-                    if(item.id === myId && item.qty === 0)
-                    CART.remove(myId);
-                });
-                // update localStorage
-                CART.sync();
-            },
-            remove(myId){
-                CART.contents = CART.contents.filter(item =>{
-                    if(item.id !== myId)
-                    return true;
-                });
-                // update localStorage
-                CART.sync();
-            },
-            empty(){
-                // empty whole cart
-                CART.contents = [];
-                // update localStorage
-                CART.sync();
-            }
+
+function promiseJax (url) {
+    return new Promise(function (resolve, reject){
+        var request = new XMLHttpRequest();
+        request.onload = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                resolve(this.responseText);
+            };  
         };
-        CART.init();
-        console.log(CART.contents);
-        addToCartButton.addEventListener("click", function() {
-            CART.add(myId);
-            console.log(CART.contents);
-        });
-
-    }    
+        request.onerror = function () {
+            reject(new Error ("Erreur Réseau"));
+        };
+        request.open("GET", url);
+        request.send();
+    });
 };
+
 var myLocalHost = "http://localhost:3000/api/cameras/" + myId;
-request.open("GET", myLocalHost);
-request.send();
 
-// Création du panier avec un objet "CART", ainsi que toutes ses fonctionnalités
-
-// const CART = {
-//     KEY: 'rfrgrdg45dg15drg1dr',
-//     contents: [],
-//     init(){ 
-//         // check localStorage and initialize the contents of CART.contents
-//         let _contents = localStorage.getItem(CART.KEY);
-//         if(_contents) {
-//             CART.contents = JSON.parse(_contents);
-//         } else {
-//             // possible to use dummy data in the array
-//             CART.contents = [];
-//             CART.sync();
-//         }
-//     },
-//     async sync() {
-//         let _cart = JSON.stringify(CART.contents);
-//         await localStorage.setItem(CART.KEY, _cart);
-//     },
-//     find(myId){
-//         // find an item in the cart by its id (some"id" might be changed according to its key in the back end or here (myId ?))
-//         let match = CART.contents.filter(item =>{
-//             if(item.id == myId)
-//             return true;
-//         });
-//         if(match && match[0])
-//         return match[0];
-//     },
-//     add(myId){
-//         // add a new item in the cart
-//         // check if the item is not already in the cart
-//         if (CART.find(myId)){
-//             CART.increase(myId, 1);
-//         } else {
-//             // in the exemple it looks for which item we want with a filter method looking all the products from the database searching for a matching id, 
-//             // but in our case it is only the product that is currently displayed on the page
-//             let obj = {
-//                 id: myId,
-//                 title: camera.name,
-//                 qty: 1,
-//                 itemPrice: camera.price,
-//             };
-//             // push the object to the contents of CART
-//             CART.contents.push(obj);
-//             // update localStorage
-//             CART.sync();
-//         }
-//     },
-//     increase(myId, qty=1){
-//         // increase the quantity of an item in the cart
-//         CART.contents = CART.contents.map(item=>{
-//             if(item.id === myId)
-//             item.qty = item.qty + qty;
-//             return item
-//         });
-//         // update localStorage
-//         CART.sync();
-//     },
-//     reduce(myId, qty=1){
-//         // increase the quantity of an item in the cart
-//         CART.contents = CART.contents.map(item=>{
-//             if(item.id === myId)
-//             item.qty = item.qty - qty;
-//             return item
-//         });
-//         // remove item from cart if the qty is 0
-//         CART.contents.forEach(async items => {
-//             if(item.id === myId && item.qty === 0)
-//             CART.remove(myId);
-//         });
-//         // update localStorage
-//         CART.sync();
-//     },
-//     remove(myId){
-//         CART.contents = CART.contents.filter(item =>{
-//             if(item.id !== myId)
-//             return true;
-//         });
-//         // update localStorage
-//         CART.sync();
-//     },
-//     empty(){
-//         // empty whole cart
-//         CART.contents = [];
-//         // update localStorage
-//         CART.sync();
-//     }
-// }
-// initialisation du panier au chargement de la page
-document.addEventListener("load", function(){
+// 2 - On définie ce que vont executer les call backs "Resolve" et "Reject" de notre promesse
+promiseJax(myLocalHost).then(function (product) {
+    var camera = JSON.parse(product);
+    imageDisplay.src = camera.imageUrl;
+    nameDisplay.innerHTML = camera.name;
+    priceDisplay.innerHTML = camera.price/100 + " €";
+    descriptionDisplay.innerHTML = "<span>Description:</span><br>" + camera.description;
+    addToCartButton.innerHTML = "Ajouter au panier";
+    specification.appendChild(addToCartButton);
+    var cameraLenses = camera.lenses;
+    cameraLenses.forEach((lense) => {
+        var option = document.createElement("option");
+        option.appendChild(document.createTextNode('New Option Text'));
+        personalisationName.innerHTML = "Choix de la lentille:";
+        personalisationDisplay.appendChild(option);
+        option.value = lense;
+        option.innerHTML = lense;
+    });
     CART.init();
     console.log(CART.contents);
-})
+    addToCartButton.addEventListener("click", function() {
+        CART.add(myId, camera);
+        console.log(CART.contents);
+    });
+}).catch(function (err){
+    console.error(err);
+});
 
-// Ajout d'un item au panier lors du click sur le bouton "ajouter au panier"
-// addToCartButton.addEventListener("click", function() {
-//     CART.add(myId);
-//     console.log(CART.contents);
-// });
+
+
+
+
+
+
+
 
 
 
