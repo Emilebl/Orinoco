39546d7c32f2542554871924e75d2eec557e570d
Emile Bl <bailay.emile@live.fr>
2020-05-01 15:49:04 +0200

    API Request wrapped in promise - on index

--- scripts/getproductlist.js
+++ scripts/getproductlist.js
@@ -2,17 +2,38 @@
 const productGrid = document.getElementById("product-grid");
 
 // Récupération de la liste de produits de la catégorie "Camera" + affichage de la liste des produits
-var request = new XMLHttpRequest();
-    request.onload = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            var productsData = JSON.parse(this.responseText);
-            console.log(productsData);
-            productsData.forEach((product) => {
-                const afficherProduit = `<div class="products"><a href="products.html?id=${product._id}"><img src="${product.imageUrl}"><div class="products-text"><h3>${product.name}</h3><p>${product.price/100} €</p><p>${product.description}</p></div>`;
-                productGrid.innerHTML += afficherProduit;
-                console.log(product._id);
-            }
-        )};
-    };
-    request.open("GET", "http://localhost:3000/api/cameras");
-    request.send();
+
+// 1 - On imbrique notre requete AJAX dans une promesse
+function promiseJax (url) {
+    return new Promise(function (resolve, reject){
+        var request = new XMLHttpRequest();
+        request.onload = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                resolve(this.responseText);
+            };
+        };
+        request.onerror = function () {
+            reject(new Error ("Erreur Réseau"));
+        };
+        request.open("GET", url );
+        request.send();
+    });
+}
+
+// 2 - On définie ce que vont executer les call backs "Resolve" et "Reject" de notre promesse
+promiseJax("http://localhost:3000/api/cameras").then(function (listeCams) {
+    var productsData = JSON.parse(listeCams);
+    console.log(productsData);
+    productsData.forEach((product) => {
+        const afficherProduit = `<div class="products"><a href="products.html?id=${product._id}"><img src="${product.imageUrl}"><div class="products-text"><h3>${product.name}</h3><p>${product.price/100} €</p><p>${product.description}</p></div>`;
+        productGrid.innerHTML += afficherProduit;
+        console.log(product._id);
+    });
+}).catch(function (err){
+    console.error(err);
+});
+
+
+    
+
+    
