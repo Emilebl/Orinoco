88bf9361f155c2ef918e651ddeeb80a649dab2ac
Emile Bl <bailay.emile@live.fr>
2020-05-02 12:48:41 +0200

    API Request wrapped in promise - on confirmation page

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -1,15 +1,40 @@
 let order = JSON.parse(localStorage.getItem("commande"));
 let totalOrderValue = JSON.parse(localStorage.getItem("totalorder"));
 let orderConfirmation = document.getElementById("orderConfirmation");
+
 // Envoi de la commande à la base de donnée
-var request = new XMLHttpRequest();
-    request.onload = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
-         var orderId = JSON.parse(this.responseText);
-            console.log(orderId);
-            orderConfirmation.innerHTML = `<h2>Merci pour votre commande !</h2><p>Pour un prix total de ${totalOrderValue} €</p><p>Votre numéro de commande est le N°${orderId.orderId}</p>`
+
+// 1 - On imbrique notre requete AJAX dans une promesse
+function promiseJax (url) {
+    return new Promise(function (resolve, reject){
+        var request = new XMLHttpRequest();
+        request.onload = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                resolve(this.responseText);
+            };
         };
-    };
-    request.open("POST", "http://localhost:3000/api/cameras/order");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify(order));
+        request.onerror = function () {
+            reject(new Error ("Erreur Réseau"));
+        };
+        request.open("POST", url);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(order));
+        
+    });
+}
+
+// 2 - On définie ce que vont executer les call backs "Resolve" et "Reject" de notre promesse
+promiseJax("http://localhost:3000/api/cameras/order").then(function (_orderRecap) {
+    var orderRecap = JSON.parse(_orderRecap);
+    console.log(orderRecap);
+    orderConfirmation.innerHTML = `<h2>Merci pour votre commande !</h2><p>Pour un prix total de ${totalOrderValue} €</p><p>Votre numéro de commande est le N°${orderRecap.orderId}</p>`
+    
+}).catch(function (err){
+    console.error(err);
+});
+
+
+
+
+
+    
